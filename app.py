3e2b829c4d314de68edaa85eac1174fe9175bd71
fielaan <ksd292@mail.ru>
2022-07-02 22:09:05 +0300

    try to fix error #7

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
 
+
 @app.route('/', methods=['post', 'get'])
 def index():
     output = "Output of your code will be shown here"
